377acc7286c6c7df4ad6b612e40716ab6a989e43
triyank.patro <triyank.patro@gmail.com>
2024-06-23 16:49:42 +0530

    first commit

---
+++ chai.js
@@ -0,0 +1,55 @@
+// const accountId = 23432;
+// let mailID = "rai@gmail.com";
+// var password = "ppp123";
+
+// console.table([accountId,mailID,password])
+
+// let str1 = "Ravi";
+// let str2 = "kumar";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// let x = 2;
+// let y = x++;
+// console.log(`x:${x},y:${y}`);
+
+// const hero = ["Shaktiman", "naagraj","batman"];
+// let obj ={
+//     name:"raj",
+//     age:15,
+// }
+
+// const name = "ravi";
+// const repocount = 78;
+
+// console.log(`my name is ${name} my repocount is ${repocount}`);
+
+const name = new String("ravikumar");
+console.log(name[2]);
+console.log(name.charAt(2));
+console.log(name.indexOf("v"));
+
+// const subsname = name.substring(1, 3);
+// console.log(subsname);
+
+// const slicename = name.slice(-5, 5);
+// console.log(slicename);
+
+// const newname = new String("      ravikumarsahu      ");
+// console.log(newname);
+// console.log(newname.trim());
+
+// const url = "https://www.programmabl234eweb.com/apis/directory";
+
+// console.log(url.replace("programmabl", "/"));
+// console.log(url.includes("apis"));//to know same keyword is there on not in url
+
+const balance = new Number(3200222);
+//const balance = 3400;
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+const number = 234.9109;
+console.log(number.toPrecision(3));
+
+console.log(balance.toLocaleString("en-IN"));  // this we are using for indian currency 
